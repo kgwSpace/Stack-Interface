b4ce664f0d60ee2d549a29246870ae3b5bfaaa41
kgwSpace <115144167+kgwSpace@users.noreply.github.com>
2022-12-22 17:43:22 +0900

    Update ExamStack2.java

--- ExamStack2.java
+++ ExamStack2.java
@@ -1,12 +1,16 @@
 import java.util.*;
+// Deque의 두가지 용도(Stack,Queue) 중 Stack 기능만을 사용하는 래핑클래스를 만들어서 사용해보자.
+// Vector 같은 방법도 있긴 한데 그건 Non-thread 방식(구형 방식)이기 때문에 오류가 생길 수 있음 
 
-//Generic I/F을 이용해서 Deque의 견고함 확보
+//Generic I/F을 이용해서 Deque의 견고함 확보(변경 방지)
 interface IStack<E>
 {
 	public boolean push(E item);
 	public E pop();
 }
 
+// 별도의 class로 감싸주어 리더빌리티를 향상
+// == 래핑클래스
 class MyStack<E> implements IStack<E>
 {
 	private Deque<E> deq;
